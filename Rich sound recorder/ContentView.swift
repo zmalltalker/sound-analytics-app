50083f65f6d9b768c7693ee89a1f2cb4ea1f2a2c
Marius Mathiesen <marius@shortcut.io>
2026-03-04 12:25:02 +0100

    Initial Commit

---
+++ Rich sound recorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rich sound recorder
+//
+//  Created by Marius Mathiesen on 04/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rich sound recorder/Rich_sound_recorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rich_sound_recorderApp.swift
+//  Rich sound recorder
+//
+//  Created by Marius Mathiesen on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Rich_sound_recorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
